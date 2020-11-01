c8266db7705dcc2bd3dda5f54b0cae1824f544aa
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 09:51:51 -0500

    Create base map

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-#TODO: Create base map
-#TODO: Create list of addresses
-#TODO: Separate addresses into meaninful categories
-#TODO: Read addresses into Python
-#TODO: Plot addresses on map
-#TODO: Create legend for categories
-#TODO: Find way to open addresses in google maps
+# TODO: Create list of addresses
+# TODO: Separate addresses into meaninful categories
+# TODO: Read addresses into Python
+# TODO: Plot addresses on map
+# TODO: Create legend for categories
+# TODO: Find way to open addresses in google maps
 
+import folium
+
+
+def create_base_map():
+    raleigh_coords = [35.84, -78.65]
+    map = folium.Map(location=raleigh_coords, zoom_start=12)
+    return map
+
+
+def main():
+    map = create_base_map()
+    map.save("Map1.html")
+
+
+main()
