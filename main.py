e9199ecb9e8fc21b5ca7272d02b4708f2bd40165
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 09:32:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#TODO: Create list of addresses
+#TODO: Separate addresses into meaninful categories
+#TODO: Read addresses into Python
+#TODO: Plot addresses on map
+#TODO: Create legend for categories
+#TODO: Find way to open addresses in google maps
