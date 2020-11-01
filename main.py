8e83ebcff5ca7e4a6ad17d512b67df9b5f9a7752
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 11:41:31 -0500

    Plot locations to map

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# TODO: Plot addresses on map
 # TODO: Change icon based on categories
 # TODO: Create legend for categories
 # TODO: Find way to open addresses in google maps
@@ -7,21 +6,20 @@ import folium
 import pandas as pd
 from geopy.geocoders import ArcGIS
 
-def add_location_to_map(map, category, location, text):
+
+def add_location_to_map(map, category, latitude, longitude, text):
     folium.Marker(
-        location=location,
-        icon=folium.Icon(icon=category, prefix='fa'),
+        location=[latitude,longitude],
+        icon=folium.Icon(icon="home", prefix='fa'),
         popup=text
     ).add_to(map)
 
-def append_full_address_to_dataframe(locations_dataframe):
-    locations_dataframe["Full Address"] = locations_dataframe.Address + ", " + locations_dataframe.City + ", " + locations_dataframe.State + " " + locations_dataframe.Zip.astype(str)
-    return locations_dataframe
 
-def create_base_map():
-    raleigh_coords = [35.84, -78.65]
-    map = folium.Map(location=raleigh_coords, zoom_start=12, tiles='openstreetmap')
-    return map
+def append_full_address_to_dataframe(locations_dataframe):
+    locations_dataframe[
+        "Full Address"] = locations_dataframe.Address + ", " + locations_dataframe.City + ", " + locations_dataframe.State + " " + locations_dataframe.Zip.astype(
+        str)
+    return locations_dataframe
 
 def append_coordinates(locations_dataframe):
     coord = ArcGIS()
@@ -30,23 +28,38 @@ def append_coordinates(locations_dataframe):
     locations_dataframe["Longitude"] = locations_dataframe.Coordinates.apply(lambda x: x.longitude)
     return locations_dataframe
 
+def create_base_map():
+    raleigh_coords = [35.84, -78.65]
+    map = folium.Map(location=raleigh_coords, zoom_start=12, tiles='openstreetmap')
+    return map
+
 def read_location_list():
     locations_df = pd.read_csv("addresses.csv")
     return locations_df
 
+def create_zip_for_iteration(locations_dataframe):
+    category = list(locations_dataframe["Category"])
+    latitude = list(locations_dataframe["Latitude"])
+    longitude = list(locations_dataframe["Longitude"])
+    text = list(locations_dataframe["Text"])
+    zipped_location_info = zip(category,latitude,longitude,text)
+    return zipped_location_info
+
 
 def main():
     map = create_base_map()
-    # Temporary variables to test add_location_to_map()
-    category = "home"
-    location = [35.84, -78.65]
-    text = "North Hills"
+    # # Temporary variables to test add_location_to_map()
+    # category = "home"
+    # location = [35.84, -78.65]
+    # text = "North Hills"
     locations_dataframe = read_location_list()
     append_full_address_to_dataframe(locations_dataframe)
     append_coordinates(locations_dataframe)
-    print(locations_dataframe)
-    # add_location_to_map(map, category, location, text)
-    # map.save("Map1.html")
+    zipped_location_info = create_zip_for_iteration(locations_dataframe)
+
+    for category, latitude, longitude, text in zipped_location_info:
+        add_location_to_map(map,category,latitude,longitude,text)
+    map.save("Map1.html")
 
 
 main()
