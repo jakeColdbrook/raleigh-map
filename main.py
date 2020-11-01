6656875566d4ae1b158f5ee3dd228d81d59b84dc
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 11:48:37 -0500

    Create icons based on category

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# TODO: Change icon based on categories
 # TODO: Create legend for categories
 # TODO: Find way to open addresses in google maps
 
@@ -9,8 +8,8 @@ from geopy.geocoders import ArcGIS
 
 def add_location_to_map(map, category, latitude, longitude, text):
     folium.Marker(
-        location=[latitude,longitude],
-        icon=folium.Icon(icon="home", prefix='fa'),
+        location=[latitude, longitude],
+        icon=folium.Icon(icon=get_icon(category), prefix='fa'),
         popup=text
     ).add_to(map)
 
@@ -21,6 +20,7 @@ def append_full_address_to_dataframe(locations_dataframe):
         str)
     return locations_dataframe
 
+
 def append_coordinates(locations_dataframe):
     coord = ArcGIS()
     locations_dataframe["Coordinates"] = locations_dataframe["Full Address"].apply(coord.geocode)
@@ -28,22 +28,34 @@ def append_coordinates(locations_dataframe):
     locations_dataframe["Longitude"] = locations_dataframe.Coordinates.apply(lambda x: x.longitude)
     return locations_dataframe
 
+
 def create_base_map():
     raleigh_coords = [35.84, -78.65]
     map = folium.Map(location=raleigh_coords, zoom_start=12, tiles='openstreetmap')
     return map
 
-def read_location_list():
-    locations_df = pd.read_csv("addresses.csv")
-    return locations_df
 
 def create_zip_for_iteration(locations_dataframe):
     category = list(locations_dataframe["Category"])
     latitude = list(locations_dataframe["Latitude"])
     longitude = list(locations_dataframe["Longitude"])
     text = list(locations_dataframe["Text"])
-    zipped_location_info = zip(category,latitude,longitude,text)
+    zipped_location_info = zip(category, latitude, longitude, text)
     return zipped_location_info
+
+
+def get_icon(category):
+    if category == "New Construction":
+        icon = "home"
+    if category == "Landmark":
+        icon = "thumb-tack"
+    if category == "Company":
+        icon = "building"
+    return icon
+
+def read_location_list():
+    locations_df = pd.read_csv("addresses.csv")
+    return locations_df
 
 
 def main():
@@ -58,7 +70,7 @@ def main():
     zipped_location_info = create_zip_for_iteration(locations_dataframe)
 
     for category, latitude, longitude, text in zipped_location_info:
-        add_location_to_map(map,category,latitude,longitude,text)
+        add_location_to_map(map, category, latitude, longitude, text)
     map.save("Map1.html")
 
 
