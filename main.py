bb48a4d5a26782edd0d5a39d4ad1cedc8e53fc5f
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 10:03:04 -0500

    Add map tile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # TODO: Create list of addresses
-# TODO: Separate addresses into meaninful categories
+# TODO: Separate addresses into meaningful categories
 # TODO: Read addresses into Python
 # TODO: Plot addresses on map
 # TODO: Create legend for categories
@@ -10,8 +10,10 @@ import folium
 
 def create_base_map():
     raleigh_coords = [35.84, -78.65]
-    map = folium.Map(location=raleigh_coords, zoom_start=12)
+    map = folium.Map(location=raleigh_coords, zoom_start=12, tiles='openstreetmap')
     return map
+
+# def add_location_to_map():
 
 
 def main():
