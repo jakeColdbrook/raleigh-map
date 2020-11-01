d1159cc586df26ac7fd83d09b6ed0c226080d999
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 10:23:30 -0500

    Create function to add location to map

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ def create_base_map():
     map = folium.Map(location=raleigh_coords, zoom_start=12, tiles='openstreetmap')
     return map
 
-# def add_location_to_map():
+
+def add_location_to_map(map, category, location, text):
+    folium.Marker(
+        location=location,
+        icon=folium.Icon(icon=category, prefix='fa'),
+        popup=text
+    ).add_to(map)
 
 
 def main():
     map = create_base_map()
+    # Temporary variables to test add_location_to_map()
+    category = "home"
+    location = [35.84, -78.65]
+    text = "North Hills"
+
+    add_location_to_map(map, category,location,text)
     map.save("Map1.html")
 
 
