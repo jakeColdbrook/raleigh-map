9ac8b42bc916bd1719e6d5ac7670328f0285c12b
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 09:35:40 -0500

    Update TODO

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#TODO: Create base map
 #TODO: Create list of addresses
 #TODO: Separate addresses into meaninful categories
 #TODO: Read addresses into Python
 #TODO: Plot addresses on map
 #TODO: Create legend for categories
 #TODO: Find way to open addresses in google maps
+
