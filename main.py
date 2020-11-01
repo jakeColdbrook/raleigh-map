ea94d02e19fa9a006877eff77b90133d389c84c1
jakeColdbrook <66574123+jakeColdbrook@users.noreply.github.com>
2020-11-01 11:17:01 -0500

    Read locations from file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# TODO: Create list of addresses
-# TODO: Separate addresses into meaningful categories
-# TODO: Read addresses into Python
+# TODO: Convert addresses to latitude and longitude
 # TODO: Plot addresses on map
+# TODO: Change icon based on categories
 # TODO: Create legend for categories
 # TODO: Find way to open addresses in google maps
 
 import folium
+import pandas as pd
 
 
 def create_base_map():
@@ -22,14 +22,20 @@ def add_location_to_map(map, category, location, text):
     ).add_to(map)
 
 
+def read_location_list():
+    location_df = pd.read_csv("addresses.csv")
+    return location_df
+
+
 def main():
     map = create_base_map()
     # Temporary variables to test add_location_to_map()
     category = "home"
     location = [35.84, -78.65]
     text = "North Hills"
+    location_dataframe = read_location_list()
 
-    add_location_to_map(map, category,location,text)
+    add_location_to_map(map, category, location, text)
     map.save("Map1.html")
 
 
